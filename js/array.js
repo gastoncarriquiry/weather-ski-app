8e0069148e8644c0bf6a9d1e7c8ba1ef53e64b27
gastoncarriquiry <tato.carri@gmail.com>
2022-08-09 20:31:51 -0300

    first commit

---
+++ js/array.js
@@ -0,0 +1,398 @@
+let arrayCiudades = [
+    {
+        localidad: "Alagna",
+        pais: "Italia",
+        val: "alagna",
+        lat: 45.854,
+        lon: 7.937,
+        src: "alagna.jpg",
+        alt: "Imagen de Alagna"
+    },
+    {
+        localidad: "Alta Badía",
+        pais: "Italia",
+        val: "altabadia",
+        lat: 46.554,
+        lon: 11.855,
+        src: "altabadia.jpg",
+        alt: "Imagen de Alta Badía"
+    },
+    {
+        localidad: "Aspen",
+        pais: "Estados Unidos",
+        val: "aspen",
+        lat: 39.191,
+        lon: -106.817,
+        src: "aspen.jpg",
+        alt: "Imagen de Aspen"
+    },
+    {
+        localidad: "Astún",
+        pais: "España",
+        val: "astun",
+        lat: 42.809,
+        lon: -0.505,
+        src: "astun.jpg",
+        alt: "Imagen de Astún"
+    },
+    {
+        localidad: "Avoriaz",
+        pais: "Suiza",
+        val: "avoriaz",
+        lat: 46.193,
+        lon: 6.770,
+        src: "avoriaz.jpg",
+        alt: "Imagen de Avoriaz"
+    },
+    {
+        localidad: "Banff",
+        pais: "Canadá",
+        val: "banff",
+        lat: 51.51,
+        lon: -0.13,
+        src: "banff.jpg",
+        alt: "Imagen de Banff"
+    },
+    {
+        localidad: "Baqueira Beret",
+        pais: "España",
+        val: "baqueira",
+        lat: 42.700,
+        lon: 0.944,
+        src: "baqueira.jpg",
+        alt: "Imagen de Baqueira Beret"
+    },
+    {
+        localidad: "Beaver Creek",
+        pais: "Estados Unidos",
+        val: "beavercreek",
+        lat: 39.631,
+        lon: -106.521,
+        src: "beavercreek.jpg",
+        alt: "Imagen de Beaver Creek"
+    },
+    {
+        localidad: "Boí Taüll",
+        pais: "España",
+        val: "boitaull",
+        lat: 42.519,
+        lon: 0.850,
+        src: "boitaull.jpg",
+        alt: "Imagen de Boí Taüll"
+    },
+    {
+        localidad: "Breckenridge Ski Resort",
+        pais: "Estados Unidos",
+        val: "breckenridge",
+        lat: 39.475,
+        lon: -106.060,
+        src: "breckenridge.jpg",
+        alt: "Imagen de Breckenridge Ski Resort"
+    },
+    {
+        localidad: "Breuil-Cervinia",
+        pais: "Italia",
+        val: "breuil",
+        lat: 45.934,
+        lon: 7.631,
+        src: "breuil.jpg",
+        alt: "Imagen de Breuil-Cervinia"
+    },
+    {
+        localidad: "Catedral Alta Patagonia",
+        pais: "Argentina",
+        val: "catedral",
+        lat: -41.163,
+        lon: -71.445,
+        src: "catedral.jpg",
+        alt: "Imagen de Catedral Alta Patagonia"
+    },
+    {
+        localidad: "Cerler-Aramón",
+        pais: "España",
+        val: "cerler",
+        lat: 42.587,
+        lon: 0.539,
+        src: "cerler.jpg",
+        alt: "Imagen de Cerler-Aramón"
+    },
+    {
+        localidad: "Cerro Bayo",
+        pais: "Argentina",
+        val: "cerrobayo",
+        lat: -40.751,
+        lon: -71.604,
+        src: "cerrobayo.jpg",
+        alt: "Imagen de Cerro Bayo"
+    },
+    {
+        localidad: "Cerro Castor",
+        pais: "Argentina",
+        val: "cerrocastor",
+        lat: -54.724,
+        lon: -68.016,
+        src: "cerrocastor.jpg",
+        alt: "Imagen de Cerro Castor"
+    },
+    {
+        localidad: "Chamonix",
+        pais: "Francia",
+        val: "chamonix",
+        lat: 51.176,
+        lon: -115.570,
+        src: "chamonix.jpg",
+        alt: "Imagen de Chamonix"
+    },
+    {
+        localidad: "Courchevel",
+        pais: "Francia",
+        val: "courchevel",
+        lat: 45.434,
+        lon: 6.636,
+        src: "courchevel.jpg",
+        alt: "Imagen de Courchevel"
+    },
+    {
+        localidad: "El Colorado",
+        pais: "Chile",
+        val: "colorado",
+        lat: -33.349,
+        lon: -70.249,
+        src: "colorado.jpg",
+        alt: "Imagen de El Colorado"
+    },
+    {
+        localidad: "Formigal-Aramón",
+        pais: "España",
+        val: "formigal",
+        lat: 42.774,
+        lon: -0.364,
+        src: "formigal.jpg",
+        alt: "Imagen de Formigal-Aramón"
+    },
+    {
+        localidad: "Ischgl",
+        pais: "Austria",
+        val: "ischgl",
+        lat: 47.0133,
+        lon: 10.291,
+        src: "ischgl.jpg",
+        alt: "Imagen de Ischgl"
+    },
+    {
+        localidad: "Kiroro Snow World",
+        pais: "Japón",
+        val: "kiroro",
+        lat: 43.067,
+        lon: 140.989,
+        src: "kiroro.jpg",
+        alt: "Imagen de Kiroro Snow World"
+    },
+    {
+        localidad: "La Molina - Masella",
+        pais: "España",
+        val: "molinamasella",
+        lat: 42.319,
+        lon: 1.893,
+        src: "molinamasella.jpg",
+        alt: "Imagen de La Molina - Masella"
+    },
+    {
+        localidad: "La Parva",
+        pais: "Chile",
+        val: "laparva",
+        lat: -33.336,
+        lon: -70.292,
+        src: "laparva.jpg",
+        alt: "Imagen de La Parva"
+    },
+    {
+        localidad: "La Plagne",
+        pais: "Francia",
+        val: "laplagne",
+        lat: 45.506,
+        lon: 6.677,
+        src: "laplagne.jpg",
+        alt: "Imagen de La Plagne"
+    },
+    {
+        localidad: "La Thuile",
+        pais: "Italia",
+        val: "lathuile",
+        lat: 45.506,
+        lon: 6.677,
+        src: "lathuile.jpg",
+        alt: "Imagen de La Thuile"
+    },
+    {
+        localidad: "Las Leñas",
+        pais: "Argentina",
+        val: "laslenas",
+        lat: -35.142,
+        lon: -70.078,
+        src: "laslenas.jpg",
+        alt: "Imagen de Las Leñas"
+    },
+    {
+        localidad: "Lenzerheide",
+        pais: "Suiza",
+        val: "lenzerheide",
+        lat: 46.728,
+        lon: 9.557,
+        src: "lenzerheide.jpg",
+        alt: "Imagen de Lenzerheide"
+    },
+    {
+        localidad: "Niseko",
+        pais: "Japón",
+        val: "niseko",
+        lat: 52.000,
+        lon: -71.999,
+        src: "niseko.jpg",
+        alt: "Imagen de Niseko"
+    },
+    {
+        localidad: "Panticosa",
+        pais: "España",
+        val: "panticosa",
+        lat: 42.702,
+        lon: -0.288,
+        src: "panticosa.jpg",
+        alt: "Imagen de Panticosa"
+    },
+    {
+        localidad: "Park City",
+        pais: "Estados Unidos",
+        val: "parkcity",
+        lat: 40.646,
+        lon: -111.498,
+        src: "parkcity.jpg",
+        alt: "Imagen de Park City"
+    },
+    {
+        localidad: "Parques de Farellones",
+        pais: "Chile",
+        val: "farellones",
+        lat: -33.350,
+        lon: -70.300,
+        src: "farellones.jpg",
+        alt: "Imagen de Parque de Farellones"
+    },
+    {
+        localidad: "Portillo",
+        pais: "Chile",
+        val: "portillo",
+        lat: -32.836,
+        lon: -70.129,
+        src: "portillo.jpg",
+        alt: "Imagen de Portillo"
+    },
+    {
+        localidad: "Quebec",
+        pais: "Canadá",
+        val: "quebec",
+        lat: 51.51,
+        lon: -0.13,
+        src: "quebec.jpg",
+        alt: "Imagen de Quebec"
+    },
+    {
+        localidad: "Rusutsu",
+        pais: "Japón",
+        val: "rusutsu",
+        lat: 42.749,
+        lon: 140.903,
+        src: "rusutsu.jpg",
+        alt: "Imagen de Rusutsu"
+    },
+    {
+        localidad: "Saint-Martin-de-Belleville",
+        pais: "Francia",
+        val: "belleville",
+        lat: 45.382,
+        lon: 6.505,
+        src: "belleville.jpg",
+        alt: "Imagen de Saint-Martin-de-Belleville"
+    },
+    {
+        localidad: "Sierra Nevada",
+        pais: "España",
+        val: "sierranevada",
+        lat: 37.095,
+        lon: -3.400,
+        src: "sierranevada.jpg",
+        alt: "Imagen de Sierra Nevada"
+    },
+    {
+        localidad: "St. Anton",
+        pais: "Austria",
+        val: "stanton",
+        lat: 47.116,
+        lon: 10.266,
+        src: "stanton.jpg",
+        alt: "Imagen de St. Anton"
+    },
+    {
+        localidad: "Vail",
+        pais: "Estados Unidos",
+        val: "vail",
+        lat: 39.644,
+        lon: -106.373,
+        src: "vail.jpg",
+        alt: "Imagen de Vail"
+    },
+    {
+        localidad: "Val Thorens",
+        pais: "Francia",
+        val: "valthorens",
+        lat: 45.298,
+        lon: 6.579,
+        src: "valthorens.jpg",
+        alt: "Imagen de Val Thorens"
+    },
+    {
+        localidad: "Valle Nevado",
+        pais: "Chile",
+        val: "vallenevado",
+        lat: -33.354,
+        lon: -70.249,
+        src: "vallenevado.jpg",
+        alt: "Imagen de Valle Nevado"
+    },
+    {
+        localidad: "Via Lattea",
+        pais: "Italia",
+        val: "vialattea",
+        lat: 44.960,
+        lon: 6.805,
+        src: "vialattea.jpg",
+        alt: "Imagen de Via Lattea"
+    },
+    {
+        localidad: "Wanaka",
+        pais: "Nueva Zelanda",
+        val: "wanaka",
+        lat: -44.700,
+        lon: 169.150,
+        src: "wanaka.jpg",
+        alt: "Imagen de Wanaka"
+    },
+    {
+        localidad: "Whistler",
+        pais: "Canadá",
+        val: "whistler",
+        lat: 50.116,
+        lon: -122.969,
+        src: "whistler.jpg",
+        alt: "Imagen de Whistler"
+    },
+    {
+        localidad: "Zermatt",
+        pais: "Suiza",
+        val: "zermatt",
+        lat: 45.990,
+        lon: 7.753,
+        src: "zermatt.jpg",
+        alt: "Imagen de Zermatt"
+    }
+];
---
+++ js/code.js
@@ -0,0 +1,316 @@
+let btn_comparar = document.querySelector("#btn_comparar");
+let destino1 = document.querySelector("#destino1");
+let destino2 = document.querySelector("#destino2");
+let ubicDestino1;
+let ubicDestino2;
+let lat1;
+let lon1;
+let lat2;
+let lon2;
+let promedioPrecipitaciones1;
+let promedioTemp1;
+let promedioPrecipitaciones2;
+let promedioTemp2;
+let promedioViento1;
+let promedioViento2;
+let prono1Sent = false;
+let prono2Sent = false;
+
+const redondearDecimales = (_valor, _digitosDecimales) => {
+    //1 digito->10
+    //2 digitos->100
+    //3 digitos->1000
+    let nroDecimales = 1;
+    for (let i = 0; i < _digitosDecimales; i++) {
+        nroDecimales = nroDecimales * 10;
+    }
+
+    let valorRedondeado = (Math.round(_valor * nroDecimales)) / nroDecimales;
+    return valorRedondeado;
+}
+
+arrayCiudades.forEach(destino => {
+    let valueDestino = destino.val;
+    let localidadDestino = destino.localidad;
+    let paisDestino = destino.pais;
+
+    destino1.insertAdjacentHTML("beforeend", `<option value="${valueDestino}">${localidadDestino}, ${paisDestino}</option>`);
+    destino2.insertAdjacentHTML("beforeend", `<option value="${valueDestino}">${localidadDestino}, ${paisDestino}</option>`);
+});
+
+const obtenerUbicacion = () => {
+    ubicDestino1 = destino1.value;
+    ubicDestino2 = destino2.value;
+
+    if (ubicDestino1 != "" && ubicDestino2 != "") {
+        destino1.style.border = "1px solid transparent";
+        destino2.style.border = "1px solid transparent";
+
+        let coordDestino1 = arrayCiudades.filter(destino => destino.val === ubicDestino1);
+        let coordDestino2 = arrayCiudades.filter(destino => destino.val === ubicDestino2);
+
+        coordDestino1.forEach(destino => {
+            lat1 = destino.lat;
+            lon1 = destino.lon;
+        })
+
+        coordDestino2.forEach(destino => {
+            lat2 = destino.lat;
+            lon2 = destino.lon;
+        })
+
+        console.log(lat1);
+        console.log(lon1);
+        console.log(lat2);
+        console.log(lon2);
+    }
+    else if (ubicDestino1 != "") { destino1.style.border = "1px solid transparent"; }
+    else if (ubicDestino2 != "") { destino2.style.border = "1px solid transparent"; }
+}
+
+const definirDia = fechaActual => {
+    let fecha = new Date(fechaActual * 1000);
+    let dias = ["Dom.", "Lun.", "Mar.", "Mie.", "Jue.", "Vie.", "Sáb."];
+    let hoy = dias[fecha.getDay()] + " " + fecha.getDate();
+    return hoy;
+}
+
+const validarSelects = () => {
+    if (ubicDestino1 != ubicDestino2) {
+        document.querySelector("#cardDestino1").innerHTML = `<div id="cont-diarios1"></div>`;
+        document.querySelector("#cardDestino2").innerHTML = `<div id="cont-diarios2"></div>`;
+        buscarPronostico1();
+        buscarPronostico2();
+        destino1.style.border = "1px solid transparent";
+        destino2.style.border = "1px solid transparent";
+    }
+    else {
+        destino1.style.border = "1px solid red";
+        destino2.style.border = "1px solid red";
+    }
+}
+
+const buscarPronostico1 = () => {
+    ubicDestino1 = destino1.value;
+    ubicDestino2 = destino2.value;
+
+    let totalTempMax1 = 0;
+    let totalPrecipitaciones1 = 0;
+    let totalViento1 = 0;
+
+    if (ubicDestino1 != "" && ubicDestino2 != "") {
+        document.querySelector("#loader").style.display = "block";
+        destino1.style.border = "1px solid transparent";
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat1}&lon=${lon1}&exclude=current,minutely,hourly,alerts&appid=7c4d35545efb8245cdb4bccb37bf97c0&lang=es&units=metric`)
+            .then(r => r.json())
+            .then(infoProno1 => {
+                document.querySelector("#loader").style.display = "none";
+                if (infoProno1.cod != "404") {
+                    document.querySelector("#error404").style.display = "none";
+                    document.querySelector("#cardDestino1").style.display = "block";
+                    console.log(infoProno1);
+                    let levantarCiudad = arrayCiudades.filter(destino => destino1.value === destino.val);
+                    document.querySelector("#cardDestino1").insertAdjacentHTML("afterbegin", `
+                        <img src="img/imgLugares/${levantarCiudad[0].src}" alt="${levantarCiudad[0].alt}" class="imgDestino">
+                        <h1 class="tituloDestino">${levantarCiudad[0].localidad}, ${levantarCiudad[0].pais}</h1>`
+                    );
+
+                    infoProno1.daily.forEach(pronostico => {
+                        document.querySelector("#cont-diarios1").insertAdjacentHTML("beforeend", `
+                        <article class="pronoDia">
+                            <div>
+                                <h2>${definirDia(pronostico.dt)}</h2>
+                                <img src="img/${pronostico.weather[0].icon}.png" alt="${pronostico.weather[0].main}" class="icon">
+                                <div>
+                                    <p>Máx: ${redondearDecimales(pronostico.temp.max, 1)} °C</p>
+                                    <p>Mín: ${redondearDecimales(pronostico.temp.min, 1)} °C</p>
+                                </div>
+                            </div>
+                            <div>
+                                <p>Vel. del viento: ${redondearDecimales(pronostico.wind_speed, 1)} m/s</p>
+                                <p>Precipitaciones: ${pronostico.pop}%</p>
+                            </div>
+                        </article>
+                        `);
+                        let tempMax = parseFloat(pronostico.temp.max);
+                        let precipitaciones = parseFloat(pronostico.pop);
+                        let viento = parseFloat(pronostico.wind_speed);
+
+                        totalTempMax1 += tempMax;
+                        totalPrecipitaciones1 += precipitaciones;
+                        totalViento1 += viento;
+                    })
+                    promedioTemp1 = redondearDecimales((totalTempMax1 / 7), 1);
+                    promedioPrecipitaciones1 = redondearDecimales((totalPrecipitaciones1 / 7), 2);
+                    promedioViento1 = redondearDecimales((totalViento1 / 7), 1);
+                    prono1Sent = true;
+                    iniciarComparacion();
+                }
+                else { document.querySelector("#error404").style.display = "inline-block"; }
+            })
+    }
+    else if (ubicDestino1 != "") { destino1.style.border = "1px solid transparent"; }
+    else { destino1.style.border = "1px solid red"; }
+}
+
+const buscarPronostico2 = () => {
+    ubicDestino1 = destino1.value;
+    ubicDestino2 = destino2.value;
+
+    let totalTempMax2 = 0;
+    let totalPrecipitaciones2 = 0;
+    let totalViento2 = 0;
+
+    if (ubicDestino2 != "" && ubicDestino1 != "") {
+        document.querySelector("#loader").style.display = "block";
+        destino2.style.border = "1px solid transparent";
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat2}&lon=${lon2}&exclude=current,minutely,hourly,alerts&appid=7c4d35545efb8245cdb4bccb37bf97c0&lang=es&units=metric`)
+            .then(r => r.json())
+            .then(infoProno2 => {
+                document.querySelector("#loader").style.display = "none";
+                document.querySelector("#cardDestino2").style.display = "block";
+                if (infoProno2.cod != "404") {
+                    document.querySelector("#error404").style.display = "none";
+                    console.log(infoProno2);
+
+                    let levantarCiudad = arrayCiudades.filter(destino => destino2.value === destino.val);
+                    document.querySelector("#cardDestino2").insertAdjacentHTML("afterbegin", `
+                        <img src="img/imgLugares/${levantarCiudad[0].src}" alt="${levantarCiudad[0].alt}" class="imgDestino">
+                        <h1 class="tituloDestino">${levantarCiudad[0].localidad}, ${levantarCiudad[0].pais}</h1>`
+                    );
+
+                    infoProno2.daily.forEach(pronostico => {
+                        document.querySelector("#cont-diarios2").insertAdjacentHTML("beforeend", `
+                        <article class="pronoDia">
+                            <div>
+                                <h2>${definirDia(pronostico.dt)}</h2>
+                                <img src="img/${pronostico.weather[0].icon}.png" alt="${pronostico.weather[0].main}" class="icon">
+                                <div>
+                                    <p>Máx: ${redondearDecimales(pronostico.temp.max, 1)} °C</p>
+                                    <p>Mín: ${redondearDecimales(pronostico.temp.min, 1)} °C</p>
+                                </div>
+                            </div>
+                            <div>
+                                <p>Vel. del viento: ${redondearDecimales(pronostico.wind_speed, 1)} m/s</p>
+                                <p>Precipitaciones: ${pronostico.pop}%</p>
+                            </div>
+                        </article>
+                        `);
+                        let tempMax = parseFloat(pronostico.temp.max);
+                        let precipitaciones = parseFloat(pronostico.pop);
+                        let viento = parseFloat(pronostico.wind_speed);
+
+                        totalTempMax2 += tempMax;
+                        totalPrecipitaciones2 += precipitaciones;
+                        totalViento2 += viento;
+                    })
+                    promedioTemp2 = redondearDecimales((totalTempMax2 / 7), 1);
+                    promedioPrecipitaciones2 = redondearDecimales((totalPrecipitaciones2 / 7), 2);
+                    promedioViento2 = redondearDecimales((totalViento2 / 7), 1);
+                    prono2Sent = true;
+                    iniciarComparacion();
+                }
+                else { document.querySelector("#error404").style.display = "inline-block"; }
+            })
+    }
+    else if (ubicDestino2 != "") { destino2.style.border = "1px solid transparent"; }
+    else { destino2.style.border = "1px solid red"; }
+}
+
+const iniciarComparacion = () => {
+    if (prono1Sent && prono2Sent) {
+        compararPronosticos();
+    }
+}
+
+const compararPronosticos = () => {
+    let levantarCiudad1 = arrayCiudades.filter(destino => destino1.value === destino.val);
+    let levantarCiudad2 = arrayCiudades.filter(destino => destino2.value === destino.val);
+    let puntos1 = 0;
+    let puntos2 = 0;
+
+    document.querySelector("#resultadoComparacion").innerHTML = `
+        <div id="param-precip">
+        </div>
+        <div id="param-temp">
+        </div>
+        <div id="param-viento">
+        </div>
+    `;
+
+    document.querySelector("#resultadoComparacion").style.display = "block";
+    if (promedioPrecipitaciones1 > promedioPrecipitaciones2) {
+        puntos1++;
+        document.querySelector("#param-precip").insertAdjacentHTML("afterbegin", `
+            <h3>Probabilidad de nevada (promedio 7 días)</h3>
+            <div>
+                <p class="mejor">${levantarCiudad1[0].localidad} <br> ${promedioPrecipitaciones1} %</p>
+                <p>${levantarCiudad2[0].localidad} <br> ${promedioPrecipitaciones2} %</p>
+            </div>
+        `);
+    } else {
+        puntos2++;
+        document.querySelector("#param-precip").insertAdjacentHTML("afterbegin", `
+            <h3>Probabilidad de nevada (promedio 7 días)</h3>
+            <div>
+                <p>${levantarCiudad1[0].localidad} <br> ${promedioPrecipitaciones1} %</p>
+                <p class="mejor">${levantarCiudad2[0].localidad} <br> ${promedioPrecipitaciones2} %</p>
+            </div>
+        `);
+    }
+    if (promedioTemp1 < promedioTemp2) {
+        puntos1++;
+        document.querySelector("#param-temp").insertAdjacentHTML("afterbegin", `
+            <h3>Temperaturas más bajas (promedio 7 días)</h3>
+            <div>
+                <p class="mejor">${levantarCiudad1[0].localidad} <br> ${promedioTemp1} °C</p>
+                <p>${levantarCiudad2[0].localidad} <br> ${promedioTemp2} °C</p>
+            </div>
+        `);
+    } else {
+        puntos2++;
+        document.querySelector("#param-temp").insertAdjacentHTML("afterbegin", `
+            <h3>Temperatura más bajas (promedio 7 días)</h3>
+            <div>
+                <p>${levantarCiudad1[0].localidad} <br> ${promedioTemp1} °C</p>
+                <p class="mejor">${levantarCiudad2[0].localidad} <br> ${promedioTemp2} °C</p>
+            </div>
+        `);
+    }
+    if (promedioViento1 < promedioViento2) {
+        puntos1++;
+        document.querySelector("#param-viento").insertAdjacentHTML("afterbegin", `
+            <h3>Menos viento (promedio 7 días)</h3>
+            <div>
+                <p class="mejor">${levantarCiudad1[0].localidad} <br> ${promedioViento1} m/s</p>
+                <p>${levantarCiudad2[0].localidad} <br> ${promedioViento2} m/s</p>
+            </div>
+        `);
+    } else {
+        puntos2++;
+        document.querySelector("#param-viento").insertAdjacentHTML("afterbegin", `
+            <h3>Menos viento (promedio 7 días)</h3>
+            <div>
+                <p>${levantarCiudad1[0].localidad} <br> ${promedioViento1} m/s</p>
+                <p class="mejor">${levantarCiudad2[0].localidad} <br> ${promedioViento2} m/s</p>
+            </div>
+        `);
+    }
+
+    if (puntos1 > puntos2) {
+        document.querySelector("#resultadoComparacion").insertAdjacentHTML("afterbegin", `
+            <h2>El destino ideal es <span>${levantarCiudad1[0].localidad}, ${levantarCiudad1[0].pais}</span></h2>
+        `);
+    }
+    else {
+        document.querySelector("#resultadoComparacion").insertAdjacentHTML("afterbegin", `
+            <h2>El destino ideal es <span>${levantarCiudad2[0].localidad}, ${levantarCiudad2[0].pais}</span></h2>
+        `);
+    }
+}
+
+destino1.addEventListener("change", obtenerUbicacion);
+destino2.addEventListener("change", obtenerUbicacion);
+btn_comparar.addEventListener("click", validarSelects);
